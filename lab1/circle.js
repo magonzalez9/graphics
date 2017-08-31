7626041e7660dedc9187a846e15332c0bc490d2e
magonzalez9 <magonzalez@willamette.edu>
2017-08-30 22:20:31 -0700

    added js code

--- lab1/circle.js
+++ lab1/circle.js
@@ -1,7 +1,24 @@
-function goToCanvas(){
+function init() { 
+    var elem = document.getElementById('myCanvas'),
+    elemLeft = elem.offsetLeft,
+    elemTop = elem.offsetTop,
+    context = elem.getContext('2d'),
+    elements = [];
 
+// Add event listener for `click` events.
+elem.addEventListener('click', function(event) {
+    var x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+    console.log(x, y);
+    elements.forEach(function(element) {
+        if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
+            document.write("click");
+        }
+    });
+
+}, false);
 }
 
-function goToGuiMouse(){
-	
+window.onload = function (){
+    init(); 
 }
